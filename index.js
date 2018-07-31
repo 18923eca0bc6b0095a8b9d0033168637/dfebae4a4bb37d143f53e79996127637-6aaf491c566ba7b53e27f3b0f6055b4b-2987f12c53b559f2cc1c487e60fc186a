907e5ea6a3f1eb1c1bd91b30b688120fb3f5ac14
18923eca0bc6b0095a8b9d0033168637 <41907777+18923eca0bc6b0095a8b9d0033168637@users.noreply.github.com>
2018-07-31 16:16:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ client.on('reconnecting', () => console.log('I am reconnecting now!'));
 
 client.on('message', msg => {
     if (msg.channel.type == "dm") {
-        msg.channel.send("I am currently offline, please DM me once I am online. Thank you!");
+        msg.author.send("I am currently offline, please DM me once I am online. Thank you!");
+		return;
     }
 });
 
