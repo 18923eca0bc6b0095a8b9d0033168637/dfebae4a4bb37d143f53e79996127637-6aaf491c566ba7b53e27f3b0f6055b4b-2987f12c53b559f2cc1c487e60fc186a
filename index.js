af1b7630a46a82433f423d51d442e7a715c390ae
18923eca0bc6b0095a8b9d0033168637 <41907777+18923eca0bc6b0095a8b9d0033168637@users.noreply.github.com>
2018-08-26 12:53:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const { Client, Util } = require('discord.js');
 const client = new Client();
 
-var prefix = "mario!";
-
-client.on('ready', () => {
-	// nothing for now
-});
-
 client.on('warn', console.warn);
 
 client.on('error', console.error);
 
-client.on('ready', () => console.log('Yo this ready!'));
+client.on('ready', () => console.log('ProBot credits hack is running...'));
 
 client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
 
@@ -20,7 +14,7 @@ client.on('reconnecting', () => console.log('I am reconnecting now!'));
 function callAPI() {
   // TO DO
   let r = Math.random().toString(36).substring(7);
-  client.channels.get(483223319361880084).send(r);
+  client.channels.get("483223319361880084").send(r);
 }
 
 var timer = setTimeout(callAPI, 500);
